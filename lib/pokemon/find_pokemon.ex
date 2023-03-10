482e269dc5a3883c422c66d8e9716a515601833e
Mohsin Khan <0xmohsinpathan@gmail.com>
2023-03-10 20:03:27 +0530

    💥 NEW: create a `defp` for get my pokemon list

--- lib/pokemon/find_pokemon.ex
+++ lib/pokemon/find_pokemon.ex
@@ -1,4 +1,24 @@
 defmodule Pokemon.Find do
+  defstruct [
+    :id,
+    :name,
+    :hp,
+    :attack,
+    :defense,
+    :special_attack,
+    :special_defense,
+    :speed,
+    :height,
+    :weight,
+    :types
+  ]
+
   def filter(search) do
+  end
+
+  defp list_of_pokemon() do
+    {:ok, response} = HTTPoison.get("https://pokeapi.co/api/v2/pokemon/?limit=1281")
+    list_of_pokemon = Poison.decode!(response.body)
+    list_of_pokemon["results"]
   end
 end
