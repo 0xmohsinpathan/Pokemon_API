6b4a09fd6654b9bf76c242c9b3aa17cdf823839f
Mohsin Khan <0xmohsinpathan@gmail.com>
2023-03-10 20:09:13 +0530

    💥 NEW: add `defp` for get my struct change.

--- lib/pokemon/find_pokemon.ex
+++ lib/pokemon/find_pokemon.ex
@@ -16,6 +16,43 @@ defmodule Pokemon.Find do
   def filter(search) do
   end
 
+  defp making_struct(abilities) do
+    id = abilities["id"]
+
+    [%{"name" => name} | _tail] = abilities["forms"]
+
+    [
+      %{"base_stat" => hp},
+      %{"base_stat" => attack},
+      %{"base_stat" => defense},
+      %{"base_stat" => specialattack},
+      %{"base_stat" => specialdefence},
+      %{"base_stat" => speed}
+    ] = abilities["stats"]
+
+    weight = abilities["weight"]
+    height = abilities["height"]
+
+    list_of_types = abilities["types"]
+    types = Enum.map(list_of_types, fn %{"type" => %{"name" => type}} -> type end)
+
+    map = %{
+      types: types,
+      weight: weight,
+      height: height,
+      name: name,
+      speed: speed,
+      special_defense: specialdefence,
+      special_attack: specialattack,
+      defense: defense,
+      attack: attack,
+      hp: hp,
+      id: id
+    }
+
+    Kernel.struct(Pokemon.Find, map)
+  end
+
   defp list_of_pokemon() do
     {:ok, response} = HTTPoison.get("https://pokeapi.co/api/v2/pokemon/?limit=1281")
     list_of_pokemon = Poison.decode!(response.body)
