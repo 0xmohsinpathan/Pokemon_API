402f16e5ae8cfde85314927741400e52c2905e3c
Mohsin Khan <0xmohsinpathan@gmail.com>
2023-03-11 07:56:48 +0530

    ✅ Adding Tests: For pokemon API

--- test/pokemon_test.exs
+++ test/pokemon_test.exs
@@ -1,8 +1,31 @@
 defmodule PokemonTest do
   use ExUnit.Case
-  doctest Pokemon
+  alias Pokemon.Find
+  doctest Pokemon.Find
 
-  test "greets the world" do
-    assert Pokemon.hello() == :world
+  describe "test Pokemon.Find.filter_pokemon/1" do
+    test "test with valid input" do
+      assert Find.filter_pokemon("ditto") == %Find{
+               id: 132,
+               name: "ditto",
+               hp: 48,
+               attack: 48,
+               defense: 48,
+               special_attack: 48,
+               special_defense: 48,
+               speed: 48,
+               height: 3,
+               weight: 40,
+               types: ["normal"]
+             }
+    end
+
+    test "test with wrong spelling" do
+      assert Find.filter_pokemon("ditt") == nil
+    end
+
+    test "test with empty string" do
+      assert Find.filter_pokemon("") == nil
+    end
   end
 end
